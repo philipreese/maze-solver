ad3c1b2a0f50a70437cc106c98b15cd571b816ef
philipreese <pbreese42@gmail.com>
2025-04-03 13:58:03 -0400

    Add ability to draw lines

--- graphics.py
+++ graphics.py
@@ -5,10 +5,10 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -21,3 +21,28 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.__point1 = point1
+        self.__point2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.__point1.x,
+            self.__point1.y,
+            self.__point2.x,
+            self.__point2.y,
+            fill=fill_color,
+            width=2,
+        )
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from graphics import Window
+from graphics import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(10, 10), Point(100, 100)), "red")
     win.wait_for_close()
 
 
