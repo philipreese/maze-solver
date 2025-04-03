89d1b8c4a6a9e6d263a12287a765cff3106672a7
philipreese <pbreese42@gmail.com>
2025-04-03 16:35:47 -0400

    add breaking of random walls to create maze

--- maze.py
+++ maze.py
@@ -1,9 +1,12 @@
+import random
 import time
 from cell import Cell
 
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+    def __init__(
+        self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None
+    ):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -15,6 +18,9 @@ class Maze:
 
         self._create_cells()
         self._break_entrance_and_exit()
+        if seed:
+            random.seed(seed)
+        self._break_walls_r(0, 0)
 
     def _create_cells(self):
         self._cells = [
@@ -47,3 +53,43 @@ class Maze:
         self._draw_cell(0, 0)
         self._cells[-1][-1].has_bottom_wall = False
         self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+
+            # determine which cell(s) to visit next
+            if i > 0 and not self._cells[i - 1][j].visited:
+                to_visit.append((i - 1, j))
+            if j > 0 and not self._cells[i][j - 1].visited:
+                to_visit.append((i, j - 1))
+            if i < len(self._cells) - 1 and not self._cells[i + 1][j].visited:
+                to_visit.append((i + 1, j))
+            if j < len(self._cells[i]) - 1 and not self._cells[i][j + 1].visited:
+                to_visit.append((i, j + 1))
+
+            # if there is nowhere to go from here just break out
+            if not to_visit:
+                self._draw_cell(i, j)
+                return
+
+            # randomly choose the next direction to go
+            new_x, new_y = to_visit[random.randint(0, len(to_visit) - 1)]
+
+            # knock out walls between this cell and the next cell(s)
+            if new_x < i:
+                self._cells[i][j].has_left_wall = False
+                self._cells[i - 1][j].has_right_wall = False
+            elif new_y < j:
+                self._cells[i][j].has_top_wall = False
+                self._cells[i][j - 1].has_bottom_wall = False
+            elif new_x > i:
+                self._cells[i][j].has_right_wall = False
+                self._cells[i + 1][j].has_left_wall = False
+            elif new_y > j:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i][j + 1].has_top_wall = False
+
+            # recursively visit the next cell
+            self._break_walls_r(new_x, new_y)
